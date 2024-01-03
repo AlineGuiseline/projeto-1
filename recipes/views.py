e2dbadf507c56a7ecfb6e3f2a882d31d2d848cf9
Aline Guiseline <alineguiseline@gmail.com>
2024-01-03 13:40:24 -0300

    change urls, create templates and fix css

--- recipes/views.py
+++ recipes/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
+from utils.recipes.factory import make_recipe
 
 def home(request):
     return render(request, 'recipes/pages/home.html', context={
-        'name': 'Aline Guiseline 2'
+        'recipes': [make_recipe() for _ in range(10)],
     })
 
-
-
-# Create your views here.
+def recipe(request, id):
+    return render(request, 'recipes/pages/recipe-view.html', context={
+        'recipe': make_recipe(),
+        'is_detail_page': True,
+    })
